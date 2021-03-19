aa048c2bfd9b8c8c9afffe6bdf3c70d842a152dd
frankthanadorn <72251422+frankthanadorn@users.noreply.github.com>
2021-03-19 23:20:07 +0700

    edit button

--- crud-redux/src/Post.js
+++ crud-redux/src/Post.js
@@ -6,7 +6,7 @@ class Post extends Component {
             <div>
                 <h2>{this.props.post.title}</h2>
                 <p>{this.props.post.message}</p>
-                <button>Edit</button>
+                <button className="edit" onClick={() => this.props.dispatch({ type: 'EDIT_POST', id: this.props.post.id})}>Edit</button>
                 <button className="delete" onClick={() => this.props.dispatch({ type: 'DELETE_POST', id: this.props.post.id })}>Delete</button>
                 
             </div>
