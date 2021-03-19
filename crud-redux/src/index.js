4c0b897f9681a0188dc120650eefe87f1370f42c
frankthanadorn <72251422+frankthanadorn@users.noreply.github.com>
2021-03-19 16:42:11 +0700

    create store

--- crud-redux/src/index.js
+++ crud-redux/src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import  {createStore} from 'redux';
+import postReducer from './reducers/postReducer';
+import {Provider} from 'react-redux';
+
+const store = createStore(postReducer);
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
