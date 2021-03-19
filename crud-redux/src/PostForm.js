8fe8381ef37b85850567af4bed95c76c66d7ab7e
frankthanadorn <72251422+frankthanadorn@users.noreply.github.com>
2021-03-19 16:48:51 +0700

    get data

--- crud-redux/src/PostForm.js
+++ crud-redux/src/PostForm.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react'
 
 export default class PostForm extends Component {
+
+    handleSubmit = (e) =>{
+        e.preventDefault();
+        const title = this.getTitle.value;
+        const message = this.getMessage.value;
+        const data = {
+            id: new Date(),
+                title,
+                message
+        }
+        console.log(data);
+    }
+
+
+
     render() {
         return (
             <div>
-                <form action="">
+               
                 <h1>Create Post</h1>
-                <input type ="text" palceholder="Enter post title" required />
+                <form onSubmit={this.handleSubmit}>
+                <input type ="text" ref={input => this.getTitle = input} placeholder="Enter post title" required />
             <br></br>
-            <textarea cols="30" rows="5" placeholder="Enter Post" required/>
+            <textarea cols="30" ref={input => this.getMessage = input} rows="5" placeholder="Enter Post" required/>
             <br></br>
             <button>Post</button>
             </form>
